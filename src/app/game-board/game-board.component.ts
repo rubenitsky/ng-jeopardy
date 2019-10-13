333d2afe62fb83f951aa792e94fef0a710e85d11
Ruben Piatnitsky <s002832@standard.com>
2019-10-12 22:14:37 -0700

    Add game board component

--- src/app/game-board/game-board.component.ts
+++ src/app/game-board/game-board.component.ts
@@ -7,6 +7,84 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameBoardComponent implements OnInit {
 
+  public categories = [
+    {
+      label: 'The Dinosaurs',
+      clues: [
+        { score: 200, clue: '', contestant: '' },
+        { score: 400, clue: '', contestant: '' },
+        { score: 600, clue: '', contestant: '' },
+        { score: 800, clue: '', contestant: '' },
+        { score: 1000, clue: '', contestant: '' },
+      ]
+    },
+    {
+      label: 'Notable Women',
+      clues: [
+        { score: 200, clue: '', contestant: '' },
+        { score: 400, clue: '', contestant: '' },
+        { score: 600, clue: '', contestant: '' },
+        { score: 800, clue: '', contestant: '' },
+        { score: 1000, clue: '', contestant: '' },
+      ]
+    },
+    {
+      label: 'Oxford English Dictionary',
+      clues: [
+        { score: 200, clue: '', contestant: '' },
+        { score: 400, clue: '', contestant: '' },
+        { score: 600, clue: '', contestant: '' },
+        { score: 800, clue: '', contestant: '' },
+        { score: 1000, clue: '', contestant: '' },
+      ]
+    },
+    {
+      label: 'Name That Instrument',
+      clues: [
+        { score: 200, clue: '', contestant: '' },
+        { score: 400, clue: '', contestant: '' },
+        { score: 600, clue: '', contestant: '' },
+        { score: 800, clue: '', contestant: '' },
+        { score: 1000, clue: '', contestant: '' },
+      ]
+    },
+    {
+      label: 'Belgium',
+      clues: [
+        { score: 200, clue: '', contestant: '' },
+        { score: 400, clue: '', contestant: '' },
+        { score: 600, clue: '', contestant: '' },
+        { score: 800, clue: '', contestant: '' },
+        { score: 1000, clue: '', contestant: '' },
+      ]
+    },
+    {
+      label: 'Composers By Country',
+      clues: [
+        { score: 200, clue: '', contestant: '' },
+        { score: 400, clue: '', contestant: '' },
+        { score: 600, clue: '', contestant: '' },
+        { score: 800, clue: '', contestant: '' },
+        { score: 1000, clue: '', contestant: '' },
+      ]
+    }
+  ];
+
+  public contestants = [
+    {
+      name: 'Scott',
+      score: 0
+    },
+    {
+      name: 'Jim',
+      score: 0
+    },
+    {
+      name: 'Susan',
+      score: 0
+    }
+  ]
+
   constructor() { }
 
   ngOnInit() {
