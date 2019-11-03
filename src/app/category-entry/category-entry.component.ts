64bfab4b700359b4d90b2ceb4b8ce11a411e22a2
Ruben Piatnitsky <s002832@standard.com>
2019-11-03 00:52:01 -0700

    Add form group array to category entry component

--- src/app/category-entry/category-entry.component.ts
+++ src/app/category-entry/category-entry.component.ts
@@ -11,19 +11,23 @@ import {FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
 export class CategoryEntryComponent implements OnInit {
 
   public CATEGORY_COUNT = CATEGORY_COUNT;
-  public HINT_VALUES = Object.keys(HintValues);
+  public HINT_VALUES = Object.keys(HintValues).filter(val => !isNaN(val));
 
   public categoryList: Array<Category> = [];
 
 
   public nameFormControl = new FormControl('', [Validators.required]);
+  public levelFormControl = new FormControl('', []);
   public hintFormControl = new FormControl('', [ Validators.required ]);
   public answerFormControl = new FormControl('', [Validators.required ]);
 
-  public categoryHintsFormArray = new FormArray([
-    this.hintFormControl,
-    this.answerFormControl
-  ]);
+  public hintAnswerFormGroup = new FormGroup({
+    'level': this.levelFormControl,
+    'hint': this.hintFormControl,
+    'answer': this.answerFormControl
+  });
+
+  public categoryHintsFormArray = new FormArray([]);
   public categoryFormGroup = new FormGroup({
     'name': this.nameFormControl,
     'hints': this.categoryHintsFormArray
@@ -32,14 +36,34 @@ export class CategoryEntryComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.setHintForm();
+  }
+
+  public setHintForm() {
+    this.HINT_VALUES.forEach(hint => {
+      this.categoryHintsFormArray.push(this.generateHintAnswerFormGroup(hint))
+    })
+  }
+
+  private generateHintAnswerFormGroup(level: string): FormGroup {
+    return new FormGroup({
+      'level': new FormControl(level),
+      'hint': new FormControl('', [Validators.required]),
+      'answer': new FormControl('', [Validators.required])
+    });
   }
 
   public addCategory(categoryFormGroup: FormGroup): void {
-    // if (categoryFormGroup.valid) {
-    //   const formValue = categoryFormGroup.value;
-    //   this.categoryList.push(new Category(formValue.name, formValue.hints))
-    //
-    // }
+    console.log(categoryFormGroup.value);
+    if (categoryFormGroup.valid) {
+      const formValue = categoryFormGroup.value;
+      this.getClueListFromHints(formValue.hints);
+      this.categoryList.push(new Category(formValue.name, formValue.hints))
+
+    }
   }
 
+  private getClueListFromHints() {
+
+  }
 }
