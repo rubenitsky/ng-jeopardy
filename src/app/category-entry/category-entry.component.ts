dfa7d8a6c5dcd32f08372d0116933d4ed946c511
Ruben Piatnitsky <s002832@standard.com>
2019-11-03 00:14:28 -0700

    Add styling and form to category entry component

--- src/app/category-entry/category-entry.component.ts
+++ src/app/category-entry/category-entry.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {CATEGORY_COUNT, HintValues} from "../shared/enums";
+import {Category} from "../shared/category";
+import {FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-category-entry',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryEntryComponent implements OnInit {
 
+  public CATEGORY_COUNT = CATEGORY_COUNT;
+  public HINT_VALUES = Object.keys(HintValues);
+
+  public categoryList: Array<Category> = [];
+
+
+  public nameFormControl = new FormControl('', [Validators.required]);
+  public hintFormControl = new FormControl('', [ Validators.required ]);
+  public answerFormControl = new FormControl('', [Validators.required ]);
+
+  public categoryHintsFormArray = new FormArray([
+    this.hintFormControl,
+    this.answerFormControl
+  ]);
+  public categoryFormGroup = new FormGroup({
+    'name': this.nameFormControl,
+    'hints': this.categoryHintsFormArray
+  });
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public addCategory(categoryFormGroup: FormGroup): void {
+    // if (categoryFormGroup.valid) {
+    //   const formValue = categoryFormGroup.value;
+    //   this.categoryList.push(new Category(formValue.name, formValue.hints))
+    //
+    // }
   }
 
 }
