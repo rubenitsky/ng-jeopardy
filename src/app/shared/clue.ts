c69d1fed50f09c1a729b0c6b9aa42500293137d1
Ruben Piatnitsky <s002832@standard.com>
2019-11-03 00:51:07 -0700

    Add answer field to clue object

--- src/app/shared/clue.ts
+++ src/app/shared/clue.ts
@@ -3,13 +3,18 @@ import {Contestant} from './contestant';
 export class Clue {
   private readonly _score: number;
   private _clue: string;
+  private _answer: string;
   private _answeredCorrectly: Contestant;
   private _isComplete: boolean;
 
-  constructor(score, clue?: string) {
+  constructor(score, clue?: string, answer?: string) {
     this._score = score;
     if (clue) {
       this._clue = clue;
+    }
+
+    if (answer) {
+      this._answer = answer;
     }
   }
 
@@ -24,6 +29,14 @@ export class Clue {
 
   set clue(value: string) {
     this._clue = value;
+  }
+
+  get answer(): string {
+    return this._answer;
+  }
+
+  set answer(value: string) {
+    this._answer = value;
   }
 
   get answeredCorrectly(): Contestant {
