72bd5ea097f9e17c40def251562867339fa0dd40
Ruben Piatnitsky <s002832@standard.com>
2019-11-02 22:48:13 -0700

    Add routes for different components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {GameBoardComponent} from './game-board/game-board.component';
 import {HintCardComponent} from "./hint-card/hint-card.component";
+import {CoverComponent} from "./cover/cover.component";
+import {ContestantEntryComponent} from "./contestant-entry/contestant-entry.component";
+import {CategoryEntryComponent} from "./category-entry/category-entry.component";
 
 
 const routes: Routes = [
   { path: 'hint', component: HintCardComponent},
+  { path: 'cover', component: CoverComponent},
+  { path: 'category', component: CategoryEntryComponent},
+  { path: 'contestants', component: ContestantEntryComponent},
   { path: '', component: GameBoardComponent}
 ];
 
