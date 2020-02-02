1970b7d38dfa4b5308d502891599108c273c9ecc
Ruben Piatnitsky <s002832@standard.com>
2020-02-01 22:41:28 -0800

    Add contestant entry form

--- src/app/contestant-entry/contestant-entry.component.ts
+++ src/app/contestant-entry/contestant-entry.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
+
 
 @Component({
   selector: 'app-contestant-entry',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContestantEntryComponent implements OnInit {
 
+  public contestantArray = new FormArray([]);
+  public contestantFormGroup: FormGroup = new FormGroup({
+    contestants: this.contestantArray
+  });
+
+
   constructor() { }
 
   ngOnInit() {
+    this.insertNewContestantFG();
+  }
+
+  public insertNewContestantFG(): void {
+    this.contestantArray.push(
+      new FormControl('', [Validators.required])
+    );
+  }
+
+  public addContestants(contestantFormGroup: FormGroup): void {
+    if (contestantFormGroup.valid) {
+      const formValue = contestantFormGroup.value;
+    }
+  }
+
+  public removeContestant(indx: number): void {
+    this.contestantArray.removeAt(indx);
+  }
+
+  public clearContestantNameValue(indx: number): void {
+    this.contestantArray.at(indx).setValue('');
   }
 
 }
